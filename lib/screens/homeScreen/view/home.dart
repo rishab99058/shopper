ca99f015f990fdcfbf09eb2d974722ba426106c6
rishab99058 <rishab99058@gmail.com>
2026-04-05 00:18:16 +0530

    Carosoul View

--- lib/screens/homeScreen/view/home.dart
+++ lib/screens/homeScreen/view/home.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:shopper/screens/homeScreen/model/appCarosoulView.dart';
 import 'package:shopper/screens/homeScreen/model/upperPart.dart';
 import 'package:shopper/utils/constants/images.dart';
 import 'package:shopper/utils/constants/sizes.dart';
@@ -17,28 +18,7 @@ class HomeScreen extends StatelessWidget {
           children: [
             UpperPart(dark: dark),
             SizedBox(height: AppSizes.spaceBtwItems),
-            CarouselSlider(
-              items: [
-                Image.network(AppImages.banner1),
-                Image.network(AppImages.banner2),
-                Image.network(AppImages.banner3),
-                Image.network(AppImages.banner4),
-              ],
-              options: CarouselOptions(
-                autoPlay: true,
-                aspectRatio: 16 / 9,
-                viewportFraction: 0.8,
-                initialPage: 0,
-                enableInfiniteScroll: true,
-                reverse: false,
-                autoPlayInterval: Duration(seconds: 3),
-                autoPlayAnimationDuration: Duration(milliseconds: 800),
-                autoPlayCurve: Curves.easeInOut,
-                enlargeCenterPage: true,
-                onPageChanged: (index, reason) {},
-                scrollDirection: Axis.horizontal,
-              ),
-            ),
+            AppCarosoulView(),
           ],
         ),
       ),
